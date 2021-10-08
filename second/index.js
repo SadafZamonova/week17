01d77e23c97d6fc066054de83808ec2cde46fa6b
SadafZamonova <84777664+SadafZamonova@users.noreply.github.com>
2021-10-09 02:46:38 +0500

    2 zadanie

---
+++ second/index.js
@@ -0,0 +1,16 @@
+let input = document.querySelector("input");
+let button = document.querySelector("button");
+let resultNode = document.querySelector(".result");
+let reg = /viagra|XXX/gim;
+
+function onClick() {
+    let value = input.value;
+    if (reg.test(value)) {
+        let result = value.replace(reg, "***");
+        resultNode.textContent = result;
+    } else {
+        resultNode.textContent = "Прошли!";
+    }
+}
+
+button.addEventListener("click", onClick);
