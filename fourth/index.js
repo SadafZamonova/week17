0cdf0abf97842b2244831f161602abb505e2b123
SadafZamonova <84777664+SadafZamonova@users.noreply.github.com>
2021-10-15 00:48:44 +0500

    4

--- fourth/index.js
+++ fourth/index.js
@@ -1,1 +1,32 @@
-document.querySelector("button");
+let clickButton = document.querySelector("button");
+clickButton.addEventListener(`click`, function () {
+    let randomNums = [];
+
+    for (let i = 0; i < 10; i++) {
+        randomNums[i] = Math.round(Math.random() * 20 - 10);
+        document.querySelector(".result").textContent = randomNums.join(" ,");
+    }
+    let sum = 0;
+    let multiply = 1;
+    for (r of randomNums) {
+        sum += r;
+        multiply *= r;
+    }
+    document.querySelector(".sum").textContent = sum;
+    document.querySelector(".multiply").textContent = multiply;
+
+
+    let arifm = +sum / randomNums.length;
+    document.querySelector(".arifm").textContent = arifm;
+
+    let maxNumber = (a, b) => {
+            return a > b ? a : b;
+        },
+        minNumber = (a, b) => {
+            return a < b ? a : b
+        };
+    document.querySelector(".max").textContent = randomNums.reduce(maxNumber);
+    document.querySelector(".min").textContent = randomNums.reduce(minNumber)
+
+
+})
